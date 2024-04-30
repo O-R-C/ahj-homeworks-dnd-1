d5e20bc6480ea08b9b9cec6d66f0bc7e0718fb5c
Andrey Semenov <semenov.orc@gmail.com>
2024-04-30 14:50:48 +0300

    add methods save load to textarea

--- src/js/Classes/Storage.js
+++ src/js/Classes/Storage.js
@@ -15,6 +15,12 @@ export default class Storage {
   #keyCards = 'cards'
 
   /**
+   * The key for storing the textarea in localStorage.
+   * @private
+   */
+  #keyTextarea = 'textarea'
+
+  /**
    * Saves the provided cards to localStorage.
    * @param {Array} cards - The array of cards to save.
    */
@@ -28,5 +34,23 @@ export default class Storage {
    */
   loadCards() {
     return JSON.parse(this.#storage.getItem(this.#keyCards))
+  }
+
+  /**
+   * Saves the provided textarea to localStorage.
+   *
+   * @param {string} textarea - The textarea to save.
+   */
+  saveTextarea(textarea) {
+    this.#storage.setItem(this.#keyTextarea, JSON.stringify(textarea))
+  }
+
+  /**
+   * Retrieves the textarea from localStorage and parses it as JSON.
+   *
+   * @return {string} The parsed textarea object.
+   */
+  loadTextarea() {
+    return JSON.parse(this.#storage.getItem(this.#keyTextarea))
   }
 }
