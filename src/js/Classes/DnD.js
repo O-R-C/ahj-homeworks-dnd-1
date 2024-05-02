bf7074f3b7826721108aa6cf4a27da32ac0ee338
Andrey Semenov <semenov.orc@gmail.com>
2024-05-02 06:34:35 +0300

    add descriptions

--- src/js/Classes/DnD.js
+++ src/js/Classes/DnD.js
@@ -1,3 +1,6 @@
+/**
+ * The DnD class.
+ */
 export default class DnD {
   #draggedCard
   #ghostCard
@@ -6,18 +9,39 @@ export default class DnD {
   #diffTop
   #diffLeft
 
+  /**
+   * Initializes a new instance of the class with the provided columns.
+   *
+   * @param {HTMLElement} columns - The container element for the drag and drop functionality.
+   */
   constructor(columns) {
     this.#container = columns
   }
 
+  /**
+   * Initializes the DnD class by adding event listeners.
+   *
+   * @return {void} This function does not return anything.
+   */
   init() {
     this.#addEventsListeners()
   }
 
+  /**
+   * Adds event listeners to the container element.
+   *
+   * @return {void} This function does not return anything.
+   */
   #addEventsListeners = () => {
     this.#container.addEventListener('mousedown', this.#onMouseDown)
   }
 
+  /**
+   * Handles the mouse down event.
+   *
+   * @param {MouseEvent} event - The mouse down event.
+   * @return {void} This function does not return anything.
+   */
   #onMouseDown = (event) => {
     event.preventDefault()
     if (event.button !== 0) return
@@ -40,17 +64,32 @@ export default class DnD {
     this.#draggedCard.replaceWith(this.#spotCard)
   }
 
+  /**
+   * Sets the styles for the ghost card.
+   *
+   * @param {MouseEvent} event - The mouse down event.
+   * @return {void} This function does not return anything.
+   */
   #setStylesGhost = (event) => {
+    // Get the bounding rectangle of the dragged card
     const { top, left } = this.#draggedCard.getBoundingClientRect()
+
+    // Calculate the different position of the ghost card
     this.#setDifferentPosition(event, top, left)
 
-    this.#ghostCard.style.height = this.#draggedCard.offsetHeight + 'px'
-    this.#ghostCard.style.width = this.#draggedCard.offsetWidth + 'px'
+    // Set the styles for the ghost card
+    this.#ghostCard.style.height = `${this.#draggedCard.offsetHeight}px`
+    this.#ghostCard.style.width = `${this.#draggedCard.offsetWidth}px`
     this.#ghostCard.style.left = `${window.scrollX + left}px`
     this.#ghostCard.style.top = `${window.scrollY + top}px`
     this.#ghostCard.classList.add('dragged')
   }
 
+  /**
+   * Sets the styles for the spot card.
+   *
+   * @return {void} This function does not return anything.
+   */
   #setStylesSpot = () => {
     this.#spotCard.style.width = this.#draggedCard.offsetWidth + 'px'
     this.#spotCard.style.height = this.#draggedCard.offsetHeight + 'px'
@@ -58,11 +97,24 @@ export default class DnD {
     this.#spotCard.style.backgroundColor = '#b3b3b3'
   }
 
+  /**
+   * Sets the different position of the ghost card.
+   *
+   * @param {MouseEvent} event - The mouse down event.
+   * @param {number} top - The top position of the dragged card.
+   * @param {number} left - The left position of the dragged card.
+   * @return {void} This function does not return anything.
+   */
   #setDifferentPosition = (event, top, left) => {
     this.#diffTop = event.pageY - top - window.scrollY
     this.#diffLeft = event.pageX - left - window.scrollX
   }
 
+  /**
+   * Sets the action listeners for the ghost card.
+   *
+   * @return {void} This function does not return anything.
+   */
   #setActionListeners = () => {
     this.#container.addEventListener('mouseup', this.#onMouseUp)
     this.#container.addEventListener('mousemove', this.#onMouseMove)
@@ -70,32 +122,68 @@ export default class DnD {
     this.#container.addEventListener('mousemove', this.#onGhostCardOver)
   }
 
+  /**
+   * Sets the position of the ghost card based on the mouse movement.
+   *
+   * @param {MouseEvent} event - The mouse move event.
+   * @return {void} This function does not return anything.
+   */
   #onMouseMove = (event) => {
     event.preventDefault()
-    if (!this.#draggedCard) return
+
+    if (!this.#draggedCard) {
+      return
+    }
 
     this.#ghostCard.style.left = `${event.pageX - this.#diffLeft}px`
     this.#ghostCard.style.top = `${event.pageY - this.#diffTop}px`
   }
 
+  /**
+   * Resets the dragged card when the mouse leaves the container.
+   *
+   * @return {void} This function does not return anything.
+   */
   #onMouseLeave = () => {
-    if (!this.#draggedCard) return
+    if (!this.#draggedCard) {
+      return
+    }
 
     this.#dropCard()
     this.#resetDraggedCard()
   }
 
+  /**
+   * Resets the dragged card when the mouse button is released.
+   *
+   * @return {void} This function does not return anything.
+   */
   #onMouseUp = () => {
-    if (!this.#draggedCard) return
+    if (!this.#draggedCard) {
+      return
+    }
 
     this.#dropCard()
     this.#resetDraggedCard()
   }
 
+  /**
+   * Replaces the dragged card with the spot card.
+   *
+   * @return {void} This function does not return anything.
+   */
   #dropCard = () => {
     this.#spotCard.replaceWith(this.#draggedCard)
   }
 
+  /**
+   * Shows the spot card in the correct position relative to the closest card.
+   *
+   * @param {HTMLElement} closestCard - The closest card element.
+   * @param {number} currentY - The current y-coordinate of the mouse.
+   * @param {HTMLElement} card - The card element to show.
+   * @return {void} This function does not return anything.
+   */
   #showSpot = (closestCard, currentY, card) => {
     const { top } = closestCard.getBoundingClientRect()
     const closestContent = closestCard.closest('[data-id="column-content"]')
@@ -107,9 +195,23 @@ export default class DnD {
       : closestContent.insertBefore(card, closestCard)
   }
 
+  /**
+   * Resets the dragged card and its auxiliary elements.
+   *
+   * This function removes the ghost card and spot card, and sets
+   * the dragged card to null.
+   *
+   * @return {void}
+   */
   #resetDraggedCard = () => {
-    this.#ghostCard && this.#ghostCard.remove()
-    this.#spotCard && this.#spotCard.remove()
+    if (this.#ghostCard) {
+      this.#ghostCard.remove()
+    }
+
+    if (this.#spotCard) {
+      this.#spotCard.remove()
+    }
+
     this.#draggedCard = null
     this.#ghostCard = null
     this.#spotCard = null
@@ -120,8 +222,19 @@ export default class DnD {
     this.#container.removeEventListener('mousemove', this.#onGhostCardOver)
   }
 
+  /**
+   * Handles the 'mouseover' event of the ghost card.
+   *
+   * If the dragged card exists, sets the cursor to grabbing and
+   * inserts the spot card before the closest card in the column.
+   *
+   * @param {MouseEvent} event - The event object.
+   * @return {void}
+   */
   #onGhostCardOver = (event) => {
-    if (!this.#draggedCard) return
+    if (!this.#draggedCard) {
+      return
+    }
 
     this.#setCursorGrabbing(event)
 
@@ -133,20 +246,40 @@ export default class DnD {
       return
     }
 
-    if (!closestCard) return
+    if (!closestCard) {
+      return
+    }
 
     this.#showSpot(closestCard, event.pageY, this.#spotCard)
   }
 
+  /**
+   * Checks if the content has cards.
+   *
+   * @param {HTMLElement} content - The content element.
+   * @return {boolean} True if the content has cards, false otherwise.
+   */
   #hasCards = (content) => {
     return content.hasChildNodes()
   }
 
+  /**
+   * Sets the cursor to grabbing.
+   *
+   * @param {MouseEvent} event - The event object.
+   * @return {void}
+   */
   #setCursorGrabbing = (event) => {
     event.target.classList.add('grabbing')
     event.target.addEventListener('mouseleave', this.#removeClassGrabbing)
   }
 
+  /**
+   * Removes the cursor from grabbing.
+   *
+   * @param {MouseEvent} event - The event object.
+   * @return {void}
+   */
   #removeClassGrabbing = (event) => {
     event.target.classList.remove('grabbing')
     event.target.removeEventListener('mouseleave', this.#removeClassGrabbing)
